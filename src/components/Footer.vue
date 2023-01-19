605b42652befecb88cf93595f894bb8d0fe72787
mrjiangyan <mrjiangyan@hotmail.com>
2023-01-19 10:10:25 +0800

    Footer的功能调整

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- ======= Footer ======= -->
   <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-    <div class="footer-newsletter">
+    <div class="footer-newsletter" v-if="false">
       <div class="container">
         <div class="row">
           <div class="col-lg-6">
@@ -26,8 +26,8 @@
               <li><i class="bx bx-chevron-right"></i> <a href="#">首页</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">关于我们</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">服务</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li> -->
             </ul>
           </div>
 
@@ -45,21 +45,21 @@
           <div class="col-lg-3 col-md-6 footer-contact">
             <h4>联系我们</h4>
             <p>
-              A108 Adam Street <br />
+              <!-- A108 Adam Street <br />
               New York, NY 535022<br />
-              United States <br /><br />
-              <strong>Phone:</strong> +139 1796 9197<br />
-              <strong>Email:</strong> mrjiangyan@hotmail.com<br />
+              United States <br /><br /> -->
+              <strong>Phone:</strong> +177 0169 4035<br />
+              <strong>Email:</strong> touchbiz@outlook.com<br />
             </p>
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>About Moderna</h3>
+          <div v-if="false" class="col-lg-3 col-md-6 footer-info">
+            <h3>About TouchBiz</h3>
             <p>
               Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta
               donna mare fermentum iaculis eu non diam phasellus.
             </p>
-            <div class="social-links mt-3">
+            <div class="social-links mt-3" v-if="false">
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
               <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
               <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
@@ -72,15 +72,8 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Moderna</span></strong
+        &copy; Copyright <strong><span>TouchBiz</span></strong
         >. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
   </footer>
@@ -89,10 +82,8 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
+  // eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
+  name: 'Footer',
 }
 </script>
 
