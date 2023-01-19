4fac7e63be1de554c90242a607705f2cfcd97ec4
mrjiangyan <mrjiangyan@hotmail.com>
2023-01-19 17:19:11 +0800

    链接选中状态的样式

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -149,3 +149,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav a.router-link-exact-active {
+  color: #a2cce3;
+}
+</style>
